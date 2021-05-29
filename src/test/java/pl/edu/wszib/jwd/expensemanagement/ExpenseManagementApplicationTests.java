a1e7b181d524fc583562e68d44d3ac63701bd444
PawelZapala <pawelzapala007@gmail.com>
2021-05-29 13:41:41 +0200

    Initial commit

---
+++ src/test/java/pl/edu/wszib/jwd/expensemanagement/ExpenseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.expensemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
